6245328bbd3e146b65692514e16f2e62412a9dde
ljx <junxun-luo@outlook.com>
2024-03-20 22:08:34 +0800

    test with mkdocs

--- site/get_nav.sh
+++ site/get_nav.sh
@@ -1,4 +1,3 @@
-
 #!/bin/bash
 IFS=$(echo -en "\n\b")
 
@@ -9,28 +8,28 @@ function getdir(){
         counter=$(echo $dir_or_file | grep -o / | wc -l)
         let counter-=2
         if [ -d "$dir_or_file" ] && [[ "$dir_or_file" != *"img"* && "$dir_or_file" != *"/img/"* ]]; then
-            printf '%0.s  ' $(seq 0 $counter) >> _sidebar.md
-            echo "- [$element]($dir_or_file/README.md)" >> _sidebar.md
+            printf '%0.s  ' $(seq 0 $counter) >> nav.md
+            echo "- [$element]($dir_or_file/README.md)" >> nav.md
             getdir "$dir_or_file"
         else
             if [[ "$dir_or_file" != *"img"* && "${dir_or_file##*.}" == "md" && "$element" != "README.md" ]]; then
                 echo $dir_or_file
-                printf '%0.s  ' $(seq 0 $counter) >> _sidebar.md
+                printf '%0.s  ' $(seq 0 $counter) >> nav.md
                 path=$(echo $dir_or_file| sed "s/[ ]/%20/g" | sed "s/[+]/%2B/g")
                 title=$(echo $element | sed "s/.md//")
-                echo "- [$title]($path)" >> _sidebar.md
+                echo "- [$title]($path)" >> nav.md
             fi
         fi
     done
 }
 
 root_dir=$(ls -d ./*/)
-:> _sidebar.md
+:> nav.md
 for dir in $root_dir
 do
     if [ "$dir" != "." ] && [ "$dir" != "./image/" ]; then
         C1=$(echo $dir | cut -f2 -d '/')
-        echo "- $C1" | cut -f2 -d '/' >> _sidebar.md
+        echo "- $C1" | cut -f2 -d '/' >> nav.md
         getdir $(echo $dir | sed 's/.$//')
     fi
 done
--- site/test.sh
+++ site/test.sh
@@ -1,36 +1,32 @@
-
 #!/bin/bash
 IFS=$(echo -en "\n\b")
 
 function getdir(){
-    for element in $(ls -1 "$1")
+    local indent="$2"
+    for element in "$1"/*
     do
-        dir_or_file="$1/$element"
-        counter=$(echo $dir_or_file | grep -o / | wc -l)
-        let counter-=2
-        if [ -d "$dir_or_file" ] && [[ "$dir_or_file" != *"img"* && "$dir_or_file" != *"/img/"* ]]; then
-            printf '%0.s  ' $(seq 0 $counter) >> _sidebar.md
-            echo "- [$element]($dir_or_file/README.md)" >> _sidebar.md
-            getdir "$dir_or_file"
-        else
-            if [[ "$dir_or_file" != *"img"* && "${dir_or_file##*.}" == "md" ]]; then
-                echo $dir_or_file
-                printf '%0.s  ' $(seq 0 $counter) >> _sidebar.md
-                path=$(echo $dir_or_file| sed "s/[ ]/%20/g" | sed "s/[+]/%2B/g")
-                title=$(echo $element | sed "s/.md//")
-                echo "- [$title]($path)" >> _sidebar.md
+        if [ -d "$element" ]; then
+            dir_name=$(basename "$element")
+            readme_file="$element/README.md"
+            if [ -f "$readme_file" ]; then
+                echo "${indent}- \"$dir_name\": \"$readme_file\""
+                getdir "$element" "  $indent"
+            else
+                if [ "$dir_name" != "img" ]; then
+                    echo "${indent}- \"$dir_name\":"
+                    getdir "$element" "  $indent"
+                fi
             fi
+        elif [[ "${element##*.}" == "md" && $(basename "$element") != "README.md" ]]; then
+            file_name=$(basename "$element" ".md")
+            echo "${indent}- \"$file_name\": \"$element\""
         fi
     done
 }
 
-root_dir=$(ls -d ./*/)
-:> _sidebar.md
-for dir in $root_dir
+echo "nav:"
+for dir in "Notebook" "Tools" "blog"
 do
-    if [ "$dir" != "." ] && [ "$dir" != "./image/" ]; then
-        C1=$(echo $dir | cut -f2 -d '/')
-        echo "- $C1" | cut -f2 -d '/' >> _sidebar.md
-        getdir $(echo $dir | sed 's/.$//')
-    fi
+    echo "- $dir:"
+    getdir "$dir" "  "
 done
